75f7f64ca023b111e352451e06e547a91186f306
grouppro365-ux <grouppro365@gmail.com>
2026-08-13 11:24:10 +0300

    Load safe extension upgrade manager

--- browser-harness-opera/bootstrap.js
+++ browser-harness-opera/bootstrap.js
@@ -1,1 +1,1 @@
-importScripts('seo_article_writer_tatyana.js','service_worker.js','target_tracker.js');
+importScripts('seo_article_writer_tatyana.js','service_worker.js','target_tracker.js','upgrade_manager.js');
